6dfe4d1ebe19547ecd3042288a9f6929ba0193d8
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2025-08-14 15:12:59 -0600

    Added Restart Play and Pause

--- 20GC-Game-3-Frogger/Assets/Scripts/Managers/UIManager.cs
+++ 20GC-Game-3-Frogger/Assets/Scripts/Managers/UIManager.cs
@@ -6,9 +6,14 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance;
 
     [Header("Replay Icons")]
-    [SerializeField] private Image playButton;
-    [SerializeField] private Image pauseButton;
-    [SerializeField] private Image restartButton;
+    [SerializeField] private Sprite playButton;
+    [SerializeField] private Sprite pauseButton;
+    [SerializeField] private Sprite rewindButton;
+    [SerializeField] private Sprite doubleRewindButton;
+
+    [Header("Replay Button Images")]
+    [SerializeField] private Image playImage;
+    [SerializeField] private Image rewindImage;
 
     private void Awake()
     {
@@ -26,11 +31,13 @@ public class UIManager : MonoBehaviour
     {
         if (!ReplayManager.Instance.GetIsReplayPlaying())
         {
+            playImage.sprite = pauseButton;
             ReplayManager.Instance.SetIsReplayPlaying(true);
             ReplayManager.Instance.StartReplay();
         }
         else
         {
+            playImage.sprite = playButton;
             ReplayManager.Instance.SetIsReplayPlaying(false);
         }
     }
@@ -38,6 +45,10 @@ public class UIManager : MonoBehaviour
     public void RestartReplay()
     {
         ReplayManager.Instance.RestartReplay();
+        if (ReplayManager.Instance.GetIsReplayPlaying())
+        {
+            ReplayManager.Instance.StartReplay();
+        }
     }
 
     public void RewindReplay()
